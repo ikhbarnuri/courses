670add8761a3a8806578ac999734ce59bfdfe00c
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2025-06-21 13:28:31 +0700

    feat(programmer-zaman-now): update Dart course files

---
+++ programmer-zaman-now/dart/belajar-dart-library/example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/src/customer.dart';
+import 'package:belajar_dart_library/src/say_hello.dart';
+
+void main(List<String> args) {
+  print(sayHello('Eko'));
+
+  var customer = Customer();
+  var product = Product();
+}
