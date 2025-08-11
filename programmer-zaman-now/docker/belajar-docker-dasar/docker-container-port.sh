c58552574da93f87743ddb736e51cb0bb77c262e
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2025-08-11 22:01:41 +0700

    feat(programmer-zaman-now): add docker course files

---
+++ programmer-zaman-now/docker/belajar-docker-dasar/docker-container-port.sh
@@ -0,0 +1,9 @@
+docker image pull nginx:latest
+
+docker container create --name contohnginx --publish 8080:80 nginx:latest
+
+docker container start contohnginx
+
+docker container stop contohnginx
+
+docker container
---
+++ programmer-zaman-now/docker/belajar-docker-dasar/docker-image.sh
@@ -0,0 +1,7 @@
+docker image ls
+
+docker image pull redis:latest
+
+docker image pull alpine:latest
+
+docker image rm alpine:latest
