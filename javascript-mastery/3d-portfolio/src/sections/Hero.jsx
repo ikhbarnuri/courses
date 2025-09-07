5732e60ba25884834379e7e3de10c0aff0dc8802
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2025-09-07 18:58:20 +0700

    feat: add AnimatedCounter component and integrate smooth scrolling in Button

--- javascript-mastery/3d-portfolio/src/sections/Hero.jsx
+++ javascript-mastery/3d-portfolio/src/sections/Hero.jsx
@@ -1,8 +1,28 @@
+import AnimatedCounter from '../components/AnimatedCounter';
 import Button from '../components/Button';
 import HeroExperience from '../components/HeroModels/HeroExperience';
 import { words } from '../contants';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 function Hero() {
+    useGSAP(() => {
+        gsap.from(
+            '.hero-text h1',
+            {
+                y: 50,
+                opacity: 0,
+            },
+            {
+                y: 0,
+                opacity: 1,
+                stagger: 0.2,
+                duration: 5,
+                ease: 'power2.inOut',
+            }
+        );
+    });
+
     return (
         <section id="hero" className="relative overflow-hidden">
             <div className="absolute top-0 left-0 z-10">
@@ -48,6 +68,8 @@ function Hero() {
                     </div>
                 </figure>
             </div>
+
+            <AnimatedCounter />
         </section>
     );
 }
