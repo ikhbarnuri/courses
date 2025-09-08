c379fe7c8ecf7888525cd84a942eb29853cd974d
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2025-09-08 17:47:37 +0700

    feat: initialize Next.js project with TypeScript, Tailwind CSS, and basic layout

---
+++ dirumahafif/belajar-nextjs/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+export default function Home() {
+    return (
+        <div className="main-heading">
+            <h1>Selamat Datang di NextJS News</h1>
+            <p className="subtitle">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quia quod laudantium officia hic
+                nostrum dolorem ipsum suscipit omnis. Quod!
+            </p>
+        </div>
+    );
+}
